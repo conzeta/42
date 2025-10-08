cc1e6ba0da2e0d2da324fe6af117dd3e4c0371a2
Luzia Duran Fuentes <lduran-f@c2r14s3.42madrid.com>
2025-10-08 12:07:54 +0200

    svihi

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: lduran-f <lduran-f@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:12:34 by lduran-f          #+#    #+#             */
-/*   Updated: 2025/09/29 16:26:57 by lduran-f         ###   ########.fr       */
+/*   Updated: 2025/10/08 11:33:04 by lduran-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	*ft_memchr(const void *s, int c, unsigned int n)
 	{
 		if (((unsigned char *) s)[i] == (unsigned char) c)
 		{
-			
+			return (s + i); //?
 		}
 		i++;
 	}
-	return ();
+	return (0);
 }
--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,18 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lduran-f <lduran-f@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 18:42:13 by lduran-f          #+#    #+#             */
-/*   Updated: 2025/07/15 19:44:07 by lduran-f         ###   ########.fr       */
+/*   Created: 2025/10/08 11:32:07 by lduran-f          #+#    #+#             */
+/*   Updated: 2025/10/08 11:53:01 by lduran-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
-void	ft_putchar(char c)
+int	ft_memcmp(const void *s1, const void *s2, unsigned int n)
 {
-	write(1, &c, 1);
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		
+	}
+}
+
+#include <string.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	e1[] = "hola caracola";
+	char	e2[] = "hola arturo";
+
+	printf("%d\n", memcmp(e1, e2, 16));
 }
