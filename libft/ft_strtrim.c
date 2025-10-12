fcd93165a7bbd39245ff19495f06f62587d03a28
conzeta <lu.duran2.2.0@gmail.com>
2025-10-12 12:45:51 +0200

    after optimi

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lduran-f <lduran-f@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 11:38:40 by lduran-f          #+#    #+#             */
-/*   Updated: 2025/10/12 11:21:43 by lduran-f         ###   ########.fr       */
+/*   Updated: 2025/10/12 12:35:34 by lduran-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*start;
-	char	*end;
-	int		i;
-	int		setlen;
+	size_t	start;
+	size_t	end;
 
-	start = (char *) s1;
-	end = (char *) s1 + ft_strlen(s1) - 1;
-	setlen = (int) ft_strlen(set);
-	i = 0;
-	while (i < setlen)
-	{
-		while (*start == set[i])
-		{
-			start++;
-			i = -1;
-		}
-		i++;
-	}
-	i = 0;
-	while (i < setlen)
-	{
-		while (*end == set[i])
-		{
-			end--;
-			i = -1;
-		}
-		i++;
-	}
-	return (ft_substr(s1, start - s1, end - start + 1));
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]) != 0)
+		start++;
+	while (end >= start && ft_strchr(set, s1[end]) != 0)
+		end--;
+	return (ft_substr(s1, start, end - start + 1));
 }
 
 /* #include <stdio.h>
