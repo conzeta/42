31b3187c14dd3f7c8fe8a722ad98baaab9376ecb
Luzia Duran Fuentes <lduran-f@c2r14s3.42madrid.com>
2025-10-21 20:41:06 +0200

    bye comment

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,12 @@
 /*   By: lduran-f <lduran-f@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:06:02 by lduran-f          #+#    #+#             */
-/*   Updated: 2025/10/21 19:07:46 by lduran-f         ###   ########.fr       */
+/*   Updated: 2025/10/21 20:40:17 by lduran-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-# define LIBFT_H //espacios??? total:34 + 9
+# define LIBFT_H
 
 # include <stddef.h>
 # include <stdlib.h>
