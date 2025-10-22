af2c1b91313573bff7b9b0e43b2be0d3c91b196f
Luzia Duran Fuentes <lduran-f@c2r14s3.42madrid.com>
2025-10-22 17:29:23 +0200

    main;)

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lduran-f <lduran-f@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:40:22 by lduran-f          #+#    #+#             */
-/*   Updated: 2025/10/22 12:21:09 by lduran-f         ###   ########.fr       */
+/*   Updated: 2025/10/22 17:28:10 by lduran-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,42 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (ret);
 }
+/*  static void	*special_toupper(void *v)
+{
+	int	i;
+	char *c = (char *)v;
+	
+	i = 0;
+	while (c[i])
+	{
+		if (c[i] > 96 && c[i] < 123)
+			c[i] -= 32;
+		i++;
+	}
+	return (c);
+}
+static void	delete(void *cont)
+{
+	free(cont);
+}
+
+#include <stdio.h>
+int main()
+{
+	t_list	*head;
+	t_list	*tail;
+	t_list	*result;
+	char	s[] = "holajhhvwu";
+	char	t[] = "bobob";
+	head = ft_lstnew(s);
+	tail = ft_lstnew(t);
+	ft_lstadd_back(&head, tail);
+	result = ft_lstmap(head, special_toupper, delete);
+
+	while(result)
+	{
+	printf("%s\n", (char *)result->content);
+	result = result->next;
+	}
+
+} */
